7ad78fb6952a826c009bf0c7e0a673f8bebe3835
duvan.botero <duvis07>
2022-12-22 15:32:36 -0500

    Se termina crud de usuarios

--- controllers/usuarios.ts
+++ controllers/usuarios.ts
@@ -28,27 +28,80 @@ export const getUsuario = async (req: Request, res: Response) => {
 //postUsuarios
 export const postUsuarios = async (req: Request, res: Response) => {
   const { body } = req;
-  res.json({
-    msg: "postUsuarios",
-    body,
-  });
+
+  try {
+    const existeEmail = await Usuario.findOne({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (existeEmail) {
+      return res.status(400).json({
+        msg: "Ya existe un usuario con el email " + body.email,
+      });
+    }
+
+    const usuario = Usuario.build(body);
+    await usuario.save();
+
+    res.json({
+      usuario,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 //putUsuarios
 export const putUsuarios = async (req: Request, res: Response) => {
   const { id } = req.params;
-  res.json({
-    msg: "putUsuarios",
-    id,
-  });
+  const { body } = req;
+
+  try {
+    const usuario = await Usuario.findByPk(id);
+    if (!usuario) {
+      return res.status(404).json({
+        msg: "No existe un usuario con el id " + id,
+      });
+    }
+
+    await usuario.update(body);
+
+    res.json({
+      usuario,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
-//deleteUsuarios
+//deleteUsuarios (borrado lógico) (cambiamos el estado a false)
 export const deleteUsuarios = async (req: Request, res: Response) => {
   const { id } = req.params;
+
+  //Verificamos si existe el usuario en la base de datos
+  const usuario = await Usuario.findByPk(id);
+  if (!usuario) {
+    return res.status(404).json({
+      msg: "No existe un usuario con el id " + id,
+    });
+  }
+
+  //  Borrado lógico (cambiamos el estado a false)
+  await usuario.update({ estado: false });
+
+  //Físicamente lo borramos de la base de datos
+  /*   await usuario.destroy(); */
+
   res.json({
-    msg: "deleteUsuarios",
-    id,
+    usuario,
   });
 };
 
--- dist/controllers/usuarios.js
+++ dist/controllers/usuarios.js
@@ -41,27 +41,71 @@ exports.getUsuario = getUsuario;
 //postUsuarios
 const postUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    res.json({
-        msg: "postUsuarios",
-        body,
-    });
+    try {
+        const existeEmail = yield usuario_1.default.findOne({
+            where: {
+                email: body.email,
+            },
+        });
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: "Ya existe un usuario con el email " + body.email,
+            });
+        }
+        const usuario = usuario_1.default.build(body);
+        yield usuario.save();
+        res.json({
+            usuario,
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Hable con el administrador",
+        });
+    }
 });
 exports.postUsuarios = postUsuarios;
 //putUsuarios
 const putUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: "putUsuarios",
-        id,
-    });
+    const { body } = req;
+    try {
+        const usuario = yield usuario_1.default.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({
+                msg: "No existe un usuario con el id " + id,
+            });
+        }
+        yield usuario.update(body);
+        res.json({
+            usuario,
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Hable con el administrador",
+        });
+    }
 });
 exports.putUsuarios = putUsuarios;
-//deleteUsuarios
+//deleteUsuarios (borrado lógico) (cambiamos el estado a false)
 const deleteUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
+    //Verificamos si existe el usuario en la base de datos
+    const usuario = yield usuario_1.default.findByPk(id);
+    if (!usuario) {
+        return res.status(404).json({
+            msg: "No existe un usuario con el id " + id,
+        });
+    }
+    //  Borrado lógico (cambiamos el estado a false)
+    yield usuario.update({ estado: false });
+    //Físicamente lo borramos de la base de datos
+    /*   await usuario.destroy(); */
     res.json({
-        msg: "deleteUsuarios",
-        id,
+        usuario,
     });
 });
 exports.deleteUsuarios = deleteUsuarios;
